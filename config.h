f83adf36c02d117d8f18445640f0b65473840085
lilischao <lilischao>
2020-09-13 20:27:35 +0800

    新增终端透明效果

--- config.h
+++ config.h
@@ -93,6 +93,9 @@ char *termname = "st-256color";
  */
 unsigned int tabspaces = 8;
 
+/* bg opacity */
+float alpha = 0.8;
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
@@ -119,6 +122,10 @@ static const char *colorname[] = {
   /* special colors */
   [256] = "#282a36", /* background */
   [257] = "#f8f8f2", /* foreground */
+
+  /* 透明插件提供的颜色 */
+  [258] = "#cccccc", /* black*/
+  [259] = "#555555", /* black*/
 };
 
 /*
